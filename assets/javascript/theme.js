62d51351093018c54bc2c014e72cabdd446ad583
Marek Erben <erben.marek@gmail.com>
2020-02-03 10:47:25 +0100

    Layouts refactoring

--- assets/javascript/theme.js
+++ assets/javascript/theme.js
@@ -1,12 +1,20 @@
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 let prevScrollPos = window.pageYOffset;
 window.onscroll = () => {
+    // show/hide menu
     let currentScrollPos = window.pageYOffset;
     if (currentScrollPos < 50 || prevScrollPos > currentScrollPos) {
         document.getElementById("header").style.top = "0";
     } else {
         document.getElementById("header").style.top = "-120px";
     }
+    // show/hide background
+    if (currentScrollPos < 100) {
+        document.getElementById("header").classList.remove("show-background");
+    } else {
+        document.getElementById("header").classList.add("show-background");
+    }
+
     prevScrollPos = currentScrollPos;
 };
 
